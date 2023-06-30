15a3296046abe1530e3cdffdad6239d46147860f
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-06-30 12:45:33 -0300

    Project created

---
+++ popular-pexels-ios/popular_pexels_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  popular_pexels_iosApp.swift
+//  popular-pexels-ios
+//
+//  Created by Bruno Duarte on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct popular_pexels_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
